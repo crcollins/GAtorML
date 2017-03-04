74ec461f81fa3426c8e6f4e5d684e6eb1cc2acd4
Christopher Collins <crcollins@cmu.edu>
2017-03-04 18:37:30 -0500

    Add scikit-learn imports

--- pred.py
+++ pred.py
@@ -3,7 +3,11 @@ import os
 import numpy
 import scipy.stats
 from scipy.spatial.distance import cdist
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
